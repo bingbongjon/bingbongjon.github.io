6a77b37a1addffcf61d3a73bfe8681512ef1d434
bingbongjon <66971212+bingbongjon@users.noreply.github.com>
2021-11-13 15:38:40 +0000

    First Commit
    Updated personal info.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,130 +16,138 @@ import macbookPro from 'assets/macbook-pro.glb';
 import portrait from 'assets/portrait.glb';
 import './index.css';
 
-const title = 'Cody Bennett | Designer + Developer';
+const title = 'Jon Chapman | Designer + Developer';
 const description =
-  'Portfolio of Cody Jason Bennett – a multidisciplinary designer & developer with a focus on motion and user experience.';
+    'Portfolio of Jon Chapman – a multidisciplinary designer & developer with a focus on motion and user experience.';
 
-const disciplines = ['Developer', 'Creator', 'Animator', 'Illustrator', 'Musician'];
+const disciplines = ['Designer', 'Musician', 'Creator'];
 
 const Home = () => {
-  const [visibleSections, setVisibleSections] = useState([]);
-  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
-  const intro = useRef();
-  const projectOne = useRef();
-  const projectTwo = useRef();
-  const about = useRef();
-  useSmoothScroll(intro, projectOne, projectTwo, about);
+    const [visibleSections, setVisibleSections] = useState([]);
+    const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+    const intro = useRef();
+    const projectOne = useRef();
+    const projectTwo = useRef();
+    const about = useRef();
+    useSmoothScroll(intro, projectOne, projectTwo, about);
 
-  useEffect(() => {
-    const revealSections = [intro, projectOne, projectTwo, about];
+    useEffect(() => {
+        const revealSections = [intro, projectOne, projectTwo, about];
 
-    const sectionObserver = new IntersectionObserver(
-      (entries, observer) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            const section = entry.target;
-            observer.unobserve(section);
-            if (visibleSections.includes(section)) return;
-            setVisibleSections(prevSections => [...prevSections, section]);
-          }
+        const sectionObserver = new IntersectionObserver(
+            (entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        const section = entry.target;
+                        observer.unobserve(section);
+                        if (visibleSections.includes(section)) return;
+                        setVisibleSections(prevSections => [...prevSections, section]);
+                    }
+                });
+            }, { rootMargin: '0px 0px -10% 0px' }
+        );
+
+        const indicatorObserver = new IntersectionObserver(
+            ([entry]) => {
+                setScrollIndicatorHidden(!entry.isIntersecting);
+            }, { rootMargin: '-100% 0px 0px 0px' }
+        );
+
+        revealSections.forEach(section => {
+            sectionObserver.observe(section.current);
         });
-      },
-      { rootMargin: '0px 0px -10% 0px' }
+
+        indicatorObserver.observe(intro.current);
+
+        return () => {
+            sectionObserver.disconnect();
+            indicatorObserver.disconnect();
+        };
+    }, [visibleSections]);
+
+    return ( <
+        div className = "home" >
+        <
+        SEO title = { title }
+        description = { description }
+        socialImage = "https://codyb.co/social-image.png" >
+        <
+        link rel = "prefetch"
+        href = { iphone11 }
+        as = "fetch"
+        crossOrigin = "" / >
+        <
+        link rel = "prefetch"
+        href = { macbookPro }
+        as = "fetch"
+        crossOrigin = "" / >
+        <
+        link rel = "prefetch"
+        href = { portrait }
+        as = "fetch"
+        crossOrigin = "" / >
+        <
+        /SEO> <
+        Intro id = "intro"
+        sectionRef = { intro }
+        disciplines = { disciplines }
+        scrollIndicatorHidden = { scrollIndicatorHidden }
+        /> <
+        ProjectSummary id = "project-1"
+        alternate sectionRef = { projectOne }
+        visible = { visibleSections.includes(projectOne.current) }
+        index = { 1 }
+        title = "Device Models"
+        description = "Design and development of a Figma plugin to create mockups with 3D device models."
+        buttonText = "View Project"
+        buttonLink = "/projects/device-models"
+        model = {
+            {
+                type: 'phone',
+                alt: "Device Model's default image",
+                textures: [{
+                        src: deviceModelsTexture,
+                        srcSet: `${deviceModelsTexture} 254w, ${deviceModelsTextureLarge} 508w`,
+                        placeholder: deviceModelsTexturePlaceholder,
+                    },
+                    {
+                        src: deviceModelsTexture,
+                        srcSet: `${deviceModelsTexture} 254w, ${deviceModelsTextureLarge} 508w`,
+                        placeholder: deviceModelsTexturePlaceholder,
+                    },
+                ],
+            }
+        }
+        /> <
+        ProjectSummary id = "project-2"
+        sectionRef = { projectTwo }
+        visible = { visibleSections.includes(projectTwo.current) }
+        index = { 2 }
+        title = "DevTech Tools"
+        description = "Creating the ultimate productivity platform."
+        buttonText = "View Project"
+        buttonLink = "/projects/devtech-tools"
+        model = {
+            {
+                type: 'laptop',
+                alt: 'DevTech Tools Landing Page',
+                textures: [{
+                    src: dttTexture,
+                    srcSet: `${dttTexture} 800w, ${dttTextureLarge} 1440w`,
+                    placeholder: dttTexturePlaceholder,
+                }, ],
+            }
+        }
+        /> <
+        Profile sectionRef = { about }
+        visible = { visibleSections.includes(about.current) }
+        id = "about" /
+        >
+        <
+        Footer / >
+        <
+        /div>
     );
-
-    const indicatorObserver = new IntersectionObserver(
-      ([entry]) => {
-        setScrollIndicatorHidden(!entry.isIntersecting);
-      },
-      { rootMargin: '-100% 0px 0px 0px' }
-    );
-
-    revealSections.forEach(section => {
-      sectionObserver.observe(section.current);
-    });
-
-    indicatorObserver.observe(intro.current);
-
-    return () => {
-      sectionObserver.disconnect();
-      indicatorObserver.disconnect();
-    };
-  }, [visibleSections]);
-
-  return (
-    <div className="home">
-      <SEO
-        title={title}
-        description={description}
-        socialImage="https://codyb.co/social-image.png"
-      >
-        <link rel="prefetch" href={iphone11} as="fetch" crossOrigin="" />
-        <link rel="prefetch" href={macbookPro} as="fetch" crossOrigin="" />
-        <link rel="prefetch" href={portrait} as="fetch" crossOrigin="" />
-      </SEO>
-      <Intro
-        id="intro"
-        sectionRef={intro}
-        disciplines={disciplines}
-        scrollIndicatorHidden={scrollIndicatorHidden}
-      />
-      <ProjectSummary
-        id="project-1"
-        alternate
-        sectionRef={projectOne}
-        visible={visibleSections.includes(projectOne.current)}
-        index={1}
-        title="Device Models"
-        description="Design and development of a Figma plugin to create mockups with 3D device models."
-        buttonText="View Project"
-        buttonLink="/projects/device-models"
-        model={{
-          type: 'phone',
-          alt: "Device Model's default image",
-          textures: [
-            {
-              src: deviceModelsTexture,
-              srcSet: `${deviceModelsTexture} 254w, ${deviceModelsTextureLarge} 508w`,
-              placeholder: deviceModelsTexturePlaceholder,
-            },
-            {
-              src: deviceModelsTexture,
-              srcSet: `${deviceModelsTexture} 254w, ${deviceModelsTextureLarge} 508w`,
-              placeholder: deviceModelsTexturePlaceholder,
-            },
-          ],
-        }}
-      />
-      <ProjectSummary
-        id="project-2"
-        sectionRef={projectTwo}
-        visible={visibleSections.includes(projectTwo.current)}
-        index={2}
-        title="DevTech Tools"
-        description="Creating the ultimate productivity platform."
-        buttonText="View Project"
-        buttonLink="/projects/devtech-tools"
-        model={{
-          type: 'laptop',
-          alt: 'DevTech Tools Landing Page',
-          textures: [
-            {
-              src: dttTexture,
-              srcSet: `${dttTexture} 800w, ${dttTextureLarge} 1440w`,
-              placeholder: dttTexturePlaceholder,
-            },
-          ],
-        }}
-      />
-      <Profile
-        sectionRef={about}
-        visible={visibleSections.includes(about.current)}
-        id="about"
-      />
-      <Footer />
-    </div>
-  );
 };
 
 export default Home;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,108 +15,148 @@ import prerender from 'utils/prerender';
 import './reset.css';
 import './index.css';
 
-const Home = lazy(() => import('pages/Home'));
-const Contact = lazy(() => import('pages/Contact'));
-const ProjectDM = lazy(() => import('pages/DeviceModels'));
-const ProjectDTT = lazy(() => import('pages/DevTechTools'));
-const Articles = lazy(() => import('pages/Articles'));
-const Page404 = lazy(() => import('pages/404'));
+const Home = lazy(() =>
+    import ('pages/Home'));
+const Contact = lazy(() =>
+    import ('pages/Contact'));
+const ProjectDM = lazy(() =>
+    import ('pages/DeviceModels'));
+const ProjectDTT = lazy(() =>
+    import ('pages/DevTechTools'));
+const Articles = lazy(() =>
+    import ('pages/Articles'));
+const Page404 = lazy(() =>
+    import ('pages/404'));
 
 export const AppContext = createContext();
 export const TransitionContext = createContext();
 
-const repoPrompt = `\u00A9 2017-${new Date().getFullYear()} Cody Bennett\n\nCheck out the source code: https://github.com/CodyJasonBennett/portfolio`;
+const repoPrompt = `\u00A9 2017-${new Date().getFullYear()} Jon Chapman\n\nCheck out the source code: https://github.com/bingbongjon/portfolio`;
 
 const App = () => {
-  const [storedTheme] = useLocalStorage('theme', 'dark');
-  const [state, dispatch] = useReducer(
-    (state, action) => {
-      const { type, value } = action;
+    const [storedTheme] = useLocalStorage('theme', 'dark');
+    const [state, dispatch] = useReducer(
+        (state, action) => {
+            const { type, value } = action;
 
-      switch (type) {
-        case 'setTheme':
-          return { ...state, theme: value };
-        case 'toggleTheme': {
-          const newThemeId = state.theme === 'dark' ? 'light' : 'dark';
-          return { ...state, theme: newThemeId };
+            switch (type) {
+                case 'setTheme':
+                    return {...state, theme: value };
+                case 'toggleTheme':
+                    {
+                        const newThemeId = state.theme === 'dark' ? 'light' : 'dark';
+                        return {...state, theme: newThemeId };
+                    }
+                case 'toggleMenu':
+                    return {...state, menuOpen: !state.menuOpen };
+                default:
+                    throw new Error();
+            }
+        }, {
+            menuOpen: false,
+            theme: 'dark',
         }
-        case 'toggleMenu':
-          return { ...state, menuOpen: !state.menuOpen };
-        default:
-          throw new Error();
-      }
-    },
-    {
-      menuOpen: false,
-      theme: 'dark',
-    }
-  );
+    );
 
-  useEffect(() => {
-    if (!prerender) {
-      console.info(`${repoPrompt}\n\n`);
-    }
+    useEffect(() => {
+        if (!prerender) {
+            console.info(`${repoPrompt}\n\n`);
+        }
 
-    window.history.scrollRestoration = 'manual';
-  }, []);
+        window.history.scrollRestoration = 'manual';
+    }, []);
 
-  useEffect(() => {
-    dispatch({ type: 'setTheme', value: storedTheme });
-  }, [storedTheme]);
+    useEffect(() => {
+        dispatch({ type: 'setTheme', value: storedTheme });
+    }, [storedTheme]);
 
-  return (
-    <AppContext.Provider value={{ ...state, dispatch }}>
-      <ThemeProvider themeId={state.theme}>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </ThemeProvider>
-    </AppContext.Provider>
-  );
+    return ( <
+        AppContext.Provider value = {
+            {...state, dispatch } } >
+        <
+        ThemeProvider themeId = { state.theme } >
+        <
+        BrowserRouter >
+        <
+        AppRoutes / >
+        <
+        /BrowserRouter> <
+        /ThemeProvider> <
+        /AppContext.Provider>
+    );
 };
 
 const AppRoutes = () => {
-  const location = useLocation();
-  const { pathname } = location;
+    const location = useLocation();
+    const { pathname } = location;
 
-  return (
-    <Fragment>
-      <Helmet>
-        <link rel="canonical" href={`https://codyb.co${pathname}`} />
-      </Helmet>
-      <VisuallyHidden showOnFocus as="a" className="skip-to-main" href="#MainContent">
-        Skip to main content
-      </VisuallyHidden>
-      <Navbar location={location} />
-      <TransitionGroup component="main" className="app" tabIndex={-1} id="MainContent">
-        <Transition
-          key={pathname}
-          timeout={msToNum(tokens.base.durationS)}
-          onEnter={reflow}
-        >
-          {status => (
-            <TransitionContext.Provider value={{ status }}>
-              <div className={classNames('app__page', `app__page--${status}`)}>
-                <Suspense fallback={<Fragment />}>
-                  <Routes key={pathname} location={location}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/projects/device-models" element={<ProjectDM />} />
-                    <Route path="/projects/devtech-tools" element={<ProjectDTT />} />
-                    <Route path="/articles/*" element={<Articles />} />
-                    <Route path="*" element={<Page404 />} />
-                  </Routes>
-                </Suspense>
-              </div>
-            </TransitionContext.Provider>
-          )}
-        </Transition>
-      </TransitionGroup>
-    </Fragment>
-  );
+    return ( <
+        Fragment >
+        <
+        Helmet >
+        <
+        link rel = "canonical"
+        href = { `https://codyb.co${pathname}` }
+        /> <
+        /Helmet> <
+        VisuallyHidden showOnFocus as = "a"
+        className = "skip-to-main"
+        href = "#MainContent" >
+        Skip to main content <
+        /VisuallyHidden> <
+        Navbar location = { location }
+        /> <
+        TransitionGroup component = "main"
+        className = "app"
+        tabIndex = {-1 }
+        id = "MainContent" >
+        <
+        Transition key = { pathname }
+        timeout = { msToNum(tokens.base.durationS) }
+        onEnter = { reflow } >
+        {
+            status => ( <
+                TransitionContext.Provider value = {
+                    { status } } >
+                <
+                div className = { classNames('app__page', `app__page--${status}`) } >
+                <
+                Suspense fallback = { < Fragment / > } >
+                <
+                Routes key = { pathname }
+                location = { location } >
+                <
+                Route path = "/"
+                element = { < Home / > }
+                /> <
+                Route path = "/contact"
+                element = { < Contact / > }
+                /> <
+                Route path = "/projects/device-models"
+                element = { < ProjectDM / > }
+                /> <
+                Route path = "/projects/devtech-tools"
+                element = { < ProjectDTT / > }
+                /> <
+                Route path = "/articles/*"
+                element = { < Articles / > }
+                /> <
+                Route path = "*"
+                element = { < Page404 / > }
+                /> <
+                /Routes> <
+                /Suspense> <
+                /div> <
+                /TransitionContext.Provider>
+            )
+        } <
+        /Transition> <
+        /TransitionGroup> <
+        /Fragment>
+    );
 };
 
 // Disable SSR globally
 export default dynamic(() => Promise.resolve(App), {
-  ssr: false,
+    ssr: false,
 });
