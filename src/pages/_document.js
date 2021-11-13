c820fc65787e7409b703a6e4023bf7893bc5ef38
bingbongjon <66971212+bingbongjon@users.noreply.github.com>
2021-11-13 15:44:44 +0000

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,18 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class _Document extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return initialProps;
-  }
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return initialProps;
+    }
 
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+    render() {
+        return ( <
+            Html lang = "en" >
+            <
+            Head >
+            <
+            script dangerouslySetInnerHTML = {
+                {
+                    __html: `
                 if (navigator.doNotTrack !== '1' && navigator.userAgent !== 'ReactSnap') {
                   const script = document.createElement('script');
                   script.src = 'https://www.googletagmanager.com/gtag/js?id=G-19ZKYVHWKV';
@@ -26,36 +28,51 @@ class _Document extends Document {
                   gtag('config', 'G-19ZKYVHWKV');
                 }
               `.replace(/\s*\n\s*/g, ''),
-            }}
-          />
-          <meta
-            name="keywords"
-            content="creative, design, designer, developer, 3d, ui, ux, animation, website, guitar, music, musician"
-          />
-          <meta name="theme-color" content="#111111" />
-          <meta name="author" content="Cody Bennett" />
-          <meta name="googlebot" content="follow, index, noarchive" />
-          <meta name="robots" content="follow, index, noarchive" />
-          <meta name="distribution" content="web" />
+                }
+            }
+            /> <
+            meta name = "keywords"
+            content = "creative, design, designer, developer, 3d, ui, ux, animation, website, music, musician" /
+            >
+            <
+            meta name = "theme-color"
+            content = "#111111" / >
+            <
+            meta name = "author"
+            content = "Jonathan Chapman" / >
+            <
+            meta name = "googlebot"
+            content = "follow, index, noarchive" / >
+            <
+            meta name = "robots"
+            content = "follow, index, noarchive" / >
+            <
+            meta name = "distribution"
+            content = "web" / >
 
-          <link rel="manifest" href="https://codyb.co/manifest.json" />
-          <link rel="shortcut icon" href="https://codyb.co/favicon.svg" />
-          <link rel="apple-touch-icon" href="https://codyb.co/icon-256.png" />
-          <link type="text/plain" rel="author" href="https://codyb.co/humans.txt" />
+            {
+                /* <link rel="manifest" href="https://codyb.co/manifest.json" />
+                          <link rel="shortcut icon" href="https://codyb.co/favicon.svg" />
+                          <link rel="apple-touch-icon" href="https://codyb.co/icon-256.png" />
+                          <link type="text/plain" rel="author" href="https://codyb.co/humans.txt" />
 
-          <meta property="og:site_name" content="codyb.co" />
-          <meta property="og:url" content="https://codyb.co" />
+                          <meta property="og:site_name" content="codyb.co" />
+                          <meta property="og:url" content="https://codyb.co" />
 
-          <meta name="twitter:site" content="@Cody_J_Bennett" />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+                          <meta name="twitter:site" content="@Cody_J_Bennett" />
+                          <meta name="twitter:card" content="summary_large_image" /> */
+            } <
+            /Head> <
+            body >
+            <
+            Main / >
+            <
+            NextScript / >
+            <
+            /body> <
+            /Html>
+        );
+    }
 }
 
 export default _Document;
